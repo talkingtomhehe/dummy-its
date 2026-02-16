b587e85b3578a56aef834964780ebf362339da01
talkingtomhehe <phankhanhnhan01@gmail.com>
2026-02-16 17:54:35 +0700

    update hieu ung chuyen trang

--- frontend/main-webapp/src/layouts/AppLayout.tsx
+++ frontend/main-webapp/src/layouts/AppLayout.tsx
@@ -1,9 +1,11 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Sidebar from "../components/navigation/Sidebar";
 import Header from "../components/navigation/Header";
 import { SidebarProvider } from "../contexts/SidebarContext";
 
 export default function AppLayout() {
+  const location = useLocation();
+
   return (
     <SidebarProvider>
       <div className="flex flex-col min-h-screen bg-neutral-50">
@@ -17,7 +19,9 @@ export default function AppLayout() {
 
           {/* Main Content Area */}
           <main className="flex-1 bg-white rounded-[12px] shadow-sm border border-neutral-100 p-4 overflow-auto">
-            <Outlet />
+            <div key={location.pathname} className="animate-page-enter">
+              <Outlet />
+            </div>
           </main>
         </div>
       </div>
